4b4138149ad089d672779cb06f8d398ec96534d7
Aleksej Kamynin <zendzirou@gmail.com>
2017-12-27 07:55:15 +0300

    Немного почистил

--- bin/today_stats.py
+++ bin/today_stats.py
@@ -8,37 +8,44 @@ from datetime import timedelta
 from window_activity import log_reader
 
 
-data = list(log_reader())
-prev = None
-for row in data:
-    if prev is None:
-        prev = row
-        continue
-    prev['duration'] = float(row['time']) - float(prev['time'])
-    prev = row
-prev['duration'] = time.time() - float(prev['time'])
-
-def keyfn(itm):
-    return itm['software']
-
-data = sorted(data, key=keyfn)
-grouped = groupby(data, key=keyfn)
-humaned = []
-for group, dat in grouped:
-    dat = list(dat)
-    seconds = int(sum([t['duration'] for t in dat]))
-    humaned.append({'name': group, 'human': timedelta(seconds=seconds), 'seconds': seconds, 'data': dat})
-
 def workspaces(data):
     """Получить только список workspace из набора"""
     return sorted(set([tt['workspace'] for tt in data]))
 
 
-for t in sorted(humaned, key=lambda t: t['seconds']):
-    if t['name']:
-        print(
-            t['name'],
-            '('+", ".join(map(str, workspaces(t['data'])))+')',
-            t['human'],
-            sep="\t"
-        )
+def print_line(itm):
+    """Напечатать одну строку статистики"""
+    print(itm['name'], itm['human'], sep="\t")
+
+
+def do_magic():
+    """Вывести статистику за сегодня"""
+    data = list(log_reader())
+    prev = None
+    for row in data:
+        if prev is None:
+            prev = row
+            continue
+        prev['duration'] = float(row['time']) - float(prev['time'])
+        prev = row
+    prev['duration'] = time.time() - float(prev['time'])
+
+    data = sorted(data, key=lambda itm: itm['software'])
+    grouped = groupby(data, key=lambda itm: itm['software'])
+    humaned = []
+    for group, dat in grouped:
+        dat = list(dat)
+        seconds = int(sum([t['duration'] for t in dat]))
+        humaned.append({
+            'name': group,
+            'human': timedelta(seconds=seconds),
+            'seconds': seconds,
+            'data': dat})
+
+    for itm in sorted(humaned, key=lambda t: t['seconds']):
+        if itm['name']:
+            print_line(itm)
+
+
+if __name__ == '__main__':
+    do_magic()
