7e875a7d987adfa39ed2453a288615ea9a94c502
Aleksej Kamynin <zendzirou@gmail.com>
2018-04-02 20:25:54 +0300

    Линтанул слегка

--- bin/math_question.py
+++ bin/math_question.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
+"""Команда для создания паузы у человека для 'подумать' """
 
 from random import randint
 from time import time
 
-start = time()
-a = randint(100,500)
-b = randint(100,500)
+def main():
+    """Спросить решение простого математического выражения"""
+    start = time()
+    var_a = randint(100, 500)
+    var_b = randint(100, 500)
 
-answer = int(input("{} + {} = ?:".format(a, b)))
+    answer = int(input("{} + {} = ?:".format(var_a, var_b)))
 
-end = time()
+    end = time()
 
-if answer != a+b:
-    print("Correct: {}".format(a+b))
+    if answer != var_a + var_b:
+        print("Correct: {}".format(var_a + var_b))
+        print('Ответ занял секунд: ', round(end - start, 1))
+        exit(1)
+
     print('Ответ занял секунд: ', round(end - start, 1))
-    exit(1)
 
-print('Ответ занял секунд: ', round(end - start, 1))
+if __name__ == '__main__':
+    main()
