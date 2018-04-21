7957b77378ae73ce0cb5b68c14917b4a119ce319
Aleksej Kamynin <zendzirou@gmail.com>
2018-04-21 16:47:57 +0300

    Переписал скрипт

---
+++ bin/add_to_unsorted_section.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Добавление строки в секцию # [unsorted] у файла"""
+
+from sys import argv
+from os.path import exists
+
+LINE = argv[1].strip()
+DEST_FILE = argv[2]
+
+UNSORTED_LINE = "# [unsorted]"
+
+def check_file_exists():
+    """Проверить существование файла"""
+    if not exists(DEST_FILE):
+        print("File '{}' not exists!".format(DEST_FILE))
+        exit(1)
+
+
+def main():
+    """Сделать всё"""
+    check_file_exists()
+    data = open(DEST_FILE).read()
+    if UNSORTED_LINE not in data:
+        data = UNSORTED_LINE+data
+
+    data = data.replace(UNSORTED_LINE, UNSORTED_LINE+'\n'+LINE)
+
+    open(DEST_FILE, 'w').write(data)
+
+
+if __name__ == '__main__':
+    main()
