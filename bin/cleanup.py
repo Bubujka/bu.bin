e373f66c3eb9ed1c2ade4801525eecbb665efa43
Aleksej Kamynin <zendzirou@gmail.com>
2022-02-19 12:22:56 +0300

    Снизил требования к себе

--- bin/cleanup.py
+++ bin/cleanup.py
@@ -304,7 +304,7 @@ def main():
     #check_files_indexed() # Переделать
     check_all_on_git()
     #check_wiki_indexed()
-    check_db_indexed()
+    #check_db_indexed()
     check_all_reps_pushed()
     exit(min(1, STATE["errors"]))
 
